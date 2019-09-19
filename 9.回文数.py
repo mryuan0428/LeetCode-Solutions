791537b71c7336a6ca27171d5e71a2c3d176566c
MR.YUAN <mryuan0428@126.com>
2019-09-19 23:55:15 +0800

    负数？false 否则%10存列表判断

---
+++ 9.回文数.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode.cn id=9 lang=python3
+#
+# [9] 回文数
+#
+# https://leetcode-cn.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (56.54%)
+# Likes:    739
+# Dislikes: 0
+# Total Accepted:    166.7K
+# Total Submissions: 294.7K
+# Testcase Example:  '121'
+#
+# 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+# 
+# 示例 1:
+# 
+# 输入: 121
+# 输出: true
+# 
+# 
+# 示例 2:
+# 
+# 输入: -121
+# 输出: false
+# 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+# 
+# 
+# 示例 3:
+# 
+# 输入: 10
+# 输出: false
+# 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+# 
+# 
+# 进阶:
+# 
+# 你能不将整数转为字符串来解决这个问题吗？
+#***思路：负数？false 否则%10存列表判断
+#
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        elif x == 0: # == not =
+            return True
+        else:#列表存每一位的数字
+            digit = []
+            while x != 0:
+                digit.append(x%10)
+                x = x//10
+            
+            l = len(digit)
+            index = 0
+            while index <= l-1-index:
+                if digit[index] != digit[l-1-index]:
+                    return False
+                index += 1
+            return True
+
+        
