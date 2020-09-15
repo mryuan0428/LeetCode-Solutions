b5c5c2738734de68c4fb7ec3687ad598b9deebab
MR.YUAN <mryuan0428@126.com>
2020-09-15 23:54:55 +0800

    ***动态规划*****

--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -26,17 +26,45 @@
 # 
 # 输入: "cbbd"
 # 输出: "bb"
-# 
-# 
-#
+'''
+动态规划：
+用 P(i,j) 表示字符串s的第 i 到 j 个字母组成的串是否为回文串：
+P(i,j) = true,如果子串[S_i:S_j]是回文串} / = false,其它情况
 
-# @lc code=start
+动态规划的状态转移方程：
+P(i, j) = P(i+1, j-1) ∧ (S_i == S_j)
+也就是说，只有 s[i+1:j-1] 是回文串，并且S_i == S_j时，s[i:j] 才会是回文串。
+
+动态规划中的边界条件，即子串的长度为 1 或 2。长度为 1 的子串显然是个回文串；
+对于长度为 2 的子串，只要它的两个字母相同，它就是一个回文串:
+P(i, i) = true
+P(i, i+1) = ( S_i == S_{i+1} )
+
+最终的答案即为所有 P(i,j)=true 中j−i+1的最大值。
+'''
+# 注意动态规划的循环顺序！！！
+# P(i, j) = P(i+1, j-1) ∧ (S_i == S_j)
+# 先Pii，后Pxx+1……
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)] #二维数组记录P(i,j)
+        ans = "" #可能s是空的
 
+        for l in range(n): #每次判断的字符串的长度
+            for i in range(n):
+                j = i + l
+                if j >= n:
+                    break
 
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i]==s[j])
+                else:
+                    dp[i][j] = (dp[i+1][j-1] and s[i]==s[j])
 
-
-        
-# @lc code=end
+                if dp[i][j] and l+1 > len(ans):
+                    ans = s[i:j+1] # ***** j+1 !!!!!!!!!
+        return ans
 
