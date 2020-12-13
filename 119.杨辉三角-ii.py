6b4af2d75a6fb12154d6dda5bfe01117db60ad8a
MR.YUAN <mryuan0428@126.com>
2020-12-13 23:21:45 +0800

    ----------空间优化-------------

---
+++ 119.杨辉三角-ii.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode.cn id=119 lang=python3
+#
+# [119] 杨辉三角 II
+#
+# https://leetcode-cn.com/problems/pascals-triangle-ii/description/
+#
+# algorithms
+# Easy (62.28%)
+# Likes:    207
+# Dislikes: 0
+# Total Accepted:    80K
+# Total Submissions: 128.4K
+# Testcase Example:  '3'
+#
+# 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+# 
+# 
+# 
+# 在杨辉三角中，每个数是它左上方和右上方的数的和。
+# 
+# 示例:
+# 
+# 输入: 3
+# 输出: [1,3,3,1]
+# 
+# 
+# 进阶：
+# 
+# 你可以优化你的算法到 O(k) 空间复杂度吗？
+# 
+#
+
+# @lc code=start
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        yang = [0, 1, 0] # rowIndex=0
+        for r in range(1, rowIndex+1):
+            for i in range(r+1):
+                yang[i] = yang[i] + yang[i+1]
+            yang.insert(0, 0)
+        return yang[1:-1]
+
+
+
+
+# @lc code=end
+
