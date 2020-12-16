2fc8fdf2db6515d76cb4e171021e01e88d38dc1f
MR.YUAN <mryuan0428@126.com>
2020-12-16 17:58:49 +0800

    ---------------单向链表的操作----------------

--- 203.移除链表元素.py
+++ 203.移除链表元素.py
@@ -32,11 +32,13 @@
 
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
+        
         # p_head指向第一个不为val的结点，且不为空
         p_head = head
         while p_head and p_head.val == val:
             p_head = p_head.next
         if not p_head: return p_head
+        # 若不尽兴上边的操作，则申请哨兵节点！！！
 
         head = p_head # 最终返回head
         while p_head.next: #next不为空
