374de451ba32621b08e44c60b46089dd0dab98dd
MR.YUAN <mryuan0428@126.com>
2020-12-17 19:57:03 +0800

    -------------------位运算------------

---
+++ 231.2-的幂.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=231 lang=python3
+#
+# [231] 2的幂
+#
+# https://leetcode-cn.com/problems/power-of-two/description/
+#
+# algorithms
+# Easy (48.88%)
+# Likes:    265
+# Dislikes: 0
+# Total Accepted:    90.4K
+# Total Submissions: 185K
+# Testcase Example:  '1'
+#
+# 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+# 
+# 示例 1:
+# 
+# 输入: 1
+# 输出: true
+# 解释: 2^0 = 1
+# 
+# 示例 2:
+# 
+# 输入: 16
+# 输出: true
+# 解释: 2^4 = 16
+# 
+# 示例 3:
+# 
+# 输入: 218
+# 输出: false
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool: # 二进制最高位为1，其余均为0
+        return n != 0 and n & (n - 1) == 0
+# @lc code=end
+
