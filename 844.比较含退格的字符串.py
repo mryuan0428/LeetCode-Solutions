293f11c337c22cc6b7e50483a3147eb58e695b8b
MR.YUAN <mryuan0428@126.com>
2021-01-30 12:59:27 +0800

    --------栈的应用----------

---
+++ 844.比较含退格的字符串.py
@@ -0,0 +1,88 @@
+#
+# @lc app=leetcode.cn id=844 lang=python3
+#
+# [844] 比较含退格的字符串
+#
+# https://leetcode-cn.com/problems/backspace-string-compare/description/
+#
+# algorithms
+# Easy (52.16%)
+# Likes:    261
+# Dislikes: 0
+# Total Accepted:    64.4K
+# Total Submissions: 123.5K
+# Testcase Example:  '"ab#c"\n"ad#c"'
+#
+# 给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
+# 
+# 注意：如果对空文本输入退格字符，文本继续为空。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入：S = "ab#c", T = "ad#c"
+# 输出：true
+# 解释：S 和 T 都会变成 “ac”。
+# 
+# 
+# 示例 2：
+# 
+# 输入：S = "ab##", T = "c#d#"
+# 输出：true
+# 解释：S 和 T 都会变成 “”。
+# 
+# 
+# 示例 3：
+# 
+# 输入：S = "a##c", T = "#a#c"
+# 输出：true
+# 解释：S 和 T 都会变成 “c”。
+# 
+# 
+# 示例 4：
+# 
+# 输入：S = "a#c", T = "b"
+# 输出：false
+# 解释：S 会变成 “c”，但 T 仍然是 “b”。
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= S.length <= 200
+# 1 <= T.length <= 200
+# S 和 T 只含有小写字母以及字符 '#'。
+# 
+# 
+# 
+# 
+# 进阶：
+# 
+# 
+# 你可以用 O(N) 的时间复杂度和 O(1) 的空间复杂度解决该问题吗？
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        S_stack = []
+        T_stack = []
+        for i in range(len(S)):
+            if S[i] == '#':
+                if len(S_stack) != 0:
+                    S_stack.pop()
+            else: S_stack.append(S[i])
+        for i in range(len(T)):
+            if T[i] == '#':
+                if len(T_stack) != 0:
+                    T_stack.pop()
+            else: T_stack.append(T[i])
+        return S_stack == T_stack
+# @lc code=end
+
